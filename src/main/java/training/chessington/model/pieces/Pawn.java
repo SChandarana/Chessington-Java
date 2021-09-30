552a7fffc942640d8b5c76c54da1c4aa458a6ca5
Shivam Chandarana <shivam.chandarana@softwire.com>
2021-09-30 10:08:45 +0100

    Make red-01 pass. Pawns can move one square

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -15,6 +15,12 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> moves = new ArrayList<>();
+        moves.add(
+                this.getColour() == PlayerColour.WHITE
+                ? new Move(from, from.plus(-1,0))
+                : new Move(from, from.plus(1, 0))
+        );
+        return moves;
     }
 }
